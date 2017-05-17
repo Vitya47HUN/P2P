297bba5cb2d2863ad8cd9560051536cad117c18d
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-17 12:05:50 +0200

    Trying to build logging still2

--- src/main/java/com/greenfox/model/ChatMessage.java
+++ src/main/java/com/greenfox/model/ChatMessage.java
@@ -1,4 +1,29 @@
 package com.greenfox.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ChatMessage {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  long id;
+
+  public ChatMessage(){
+  }
+
+  public ChatMessage(long id){
+    this.id = id;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
 }
